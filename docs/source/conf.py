f3791a4b5c67dda45efbc4641f5ee1f1d84123c0
Koen Vervloesem <koen@vervloesem.eu>
2019-03-17 15:15:01 +0100

    Docs: Show base classes in autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -200,3 +200,8 @@ todo_include_todos = True
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 napoleon_include_init_with_doc = True
+
+# -- Options for autodoc extension ------------------------------------------
+autodoc_default_options = {
+    'inherited-members': True
+}
