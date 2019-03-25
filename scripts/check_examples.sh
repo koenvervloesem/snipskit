7fad6c9669d8f74799ba1f541f23ec043f0b24bd
Koen Vervloesem <koen@vervloesem.eu>
2019-03-25 23:18:28 +0100

    Tests: Clean up check_examples.sh

--- scripts/check_examples.sh
+++ scripts/check_examples.sh
@@ -2,9 +2,9 @@
 cd examples
 
 # Extract the example from README.rst
-tail -n +`grep -n 'code-block:: python' ../README.rst | cut -d: -f1` ../README.rst > README.example
+tail -n +$(grep -n 'code-block:: python' ../README.rst | cut -d: -f1) ../README.rst > README.example
 # The following line needs GNU head. Use tail -r | tail -n +3 | tail -r instead of head -n -2 on BSD systems.
-head -n `grep -n 'end-code-block' README.example | cut -d: -f1` README.example | tail -n +3 | head -n -2 | sed 's/^    //' > README.py
+head -n $(grep -n 'end-code-block' README.example | cut -d: -f1) README.example | tail -n +3 | head -n -2 | sed 's/^    //' > README.py
 
 # Check the examples in the examples directory.
 pylint -E *.py
