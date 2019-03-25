4bcce962b18cdb4bf2488b4c1a1494fa48c340b0
Koen Vervloesem <koen@vervloesem.eu>
2019-03-25 13:53:16 +0100

    Tests: Make modular tests possible

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+try:
+    # This environment variable is used by Travis CI to define which
+    # dependecies are installed. Pytest uses it to define which modules
+    # are checked.
+    requirements = os.environ['SNIPSKIT_REQUIREMENTS']
+
+    if requirements == 'common':
+        collect_ignore = ['mqtt', 'hermes']
+    elif requirements == 'mqtt':
+        collect_ignore = ['hermes']
+    elif requirements == 'hermes':
+        collect_ignore = ['mqtt']
+    elif requirements == 'all':
+    # Run all the tests
+        pass
+    else:
+        sys.exit('Unkown value for SNIPSKIT_REQUIREMENTS environment variable: {}'.format(requirements))
+except KeyError:
+    # Run all the tests
+    pass
