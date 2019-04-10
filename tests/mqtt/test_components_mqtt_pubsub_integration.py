475cd1c14359943227b332eac9bfde98ffd875a1
Koen Vervloesem <koen@vervloesem.eu>
2019-04-10 22:54:35 +0200

    Tests: Increase delay for MQTT messages

--- tests/mqtt/test_components_mqtt_pubsub_integration.py
+++ tests/mqtt/test_components_mqtt_pubsub_integration.py
@@ -21,7 +21,7 @@ from snipskit.mqtt.decorators import topic
 pytestmark = pytest.mark.skipif(not os.environ.get('INTEGRATION_TESTS'),
                                 reason='Integration test')
 # Delay between subscribing and publishing an MQTT message.
-DELAY=0.1
+DELAY=0.2
 
 class DecoratedMQTTComponentPubSub(MQTTSnipsComponent):
     """A simple Snips component using MQTT directly to test."""
